7088b40beb50b09f5093d99d0bdee7eb2c1fc09e
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-26 00:52:09 +0100

    Add singly linked list

---
+++ DataStructures.Tests/Lists/SinglyLinkedListTests.cs
@@ -0,0 +1,302 @@
+﻿namespace DataStructures.Tests.Lists
+{
+    using System;
+    using DataStructures.Lists;
+    using NUnit.Framework;
+    using static DataStructures.Lists.SinglyLinkedList<int>;
+
+    public class SinglyLinkedListTests
+    {
+        private SinglyLinkedList<int> _list;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _list = new SinglyLinkedList<int>();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void GetAt_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _list.GetAt(index));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void GetAt_WhenIndexIsNotOutOfRange_ShouldGetNodeAtCorrectIndex(int index)
+        {
+            // Arrange
+            for (var i = 0; i <= 10; i++)
+            {
+                _list.AddLast(i);
+            }
+
+            // Act
+            var result = _list.GetAt(index);
+
+            // Assert
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(index));
+        }
+
+        [Test]
+        [TestCase(-1, 1)]
+        [TestCase(100, 1)]
+        public void SetAt_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index, int value)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _list.SetAt(index, value));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(5, 100)]
+        [TestCase(10, 100)]
+        public void SetAt_WhenIndexIsNotOutOfRange_ShouldSetNodeAtCorrectIndex(int index, int value)
+        {
+            // Arrange
+            for (var i = 0; i <= 10; i++)
+            {
+                _list.AddLast(i);
+            }
+
+            // Act
+            _list.SetAt(index, value);
+
+            // Assert
+            var result = _list.GetAt(index);
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(value));
+        }
+
+        [Test]
+        [TestCase(-1, 1)]
+        [TestCase(100, 1)]
+        public void AddAt_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index, int value)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _list.AddAt(index, value));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(5, 100)]
+        [TestCase(10, 100)]
+        public void AddAt_WhenIndexIsNotOutOfRange_ShouldAddNodeToCorrectIndexAndIncrementSize(int index, int value)
+        {
+            // Arrange
+            for (var i = 0; i < 10; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+            var next = index == previousSize ? null : _list.GetAt(index);
+
+            // Act
+            _list.AddAt(index, value);
+
+            // Assert
+            var result = _list.GetAt(index);
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(value));
+            Assert.That(result.Next, Is.EqualTo(next));
+            Assert.That(_list.Size, Is.EqualTo(previousSize + 1));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(5, 100)]
+        [TestCase(10, 100)]
+        public void AddFirst_WhenCalled_ShouldAddNodeToHeadAndIncrementSize(int range, int value)
+        {
+            // Arrange
+            for (var i = 0; i < range; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+            var next = range == 0 ? null : _list.GetAt(0);
+
+            // Act
+            _list.AddFirst(value);
+
+            // Assert
+            var result = _list.GetAt(0);
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(value));
+            Assert.That(result.Next, Is.EqualTo(next));
+            Assert.That(_list.Size, Is.EqualTo(previousSize + 1));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(5, 100)]
+        [TestCase(10, 100)]
+        public void AddLast_WhenCalled_ShouldAddNodeToTailAndIncrementSize(int range, int value)
+        {
+            // Arrange
+            for (var i = 0; i < range; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+
+            // Act
+            _list.AddLast(value);
+
+            // Assert
+            var result = _list.GetAt(_list.Size - 1);
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(value));
+            Assert.That(result.Next, Is.EqualTo(null));
+            Assert.That(_list.Size, Is.EqualTo(previousSize + 1));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void RemoveAt_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _list.RemoveAt(index));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void RemoveAt_WhenIndexIsNotOutOfRange_ShouldRemoveNodeFromCorrectIndexAndDecrementSize(int index)
+        {
+            // Arrange
+            for (var i = 0; i <= 10; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+
+            // Act
+            var result = _list.RemoveAt(index);
+
+            // Assert
+            var previous = index == 0 ? null : _list.GetAt(index - 1);
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(Node<int>.Value)).EqualTo(index));
+            Assert.That(previous, Is.Not.EqualTo(result));
+            Assert.That(_list.Size, Is.EqualTo(previousSize - 1));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void RemoveFirst_WhenCalled_ShouldRemoveNodeFromHeadAndDecrementSize(int range)
+        {
+            // Arrange
+            for (var i = 0; i < range; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+
+            // Act
+            var result = _list.RemoveFirst();
+
+            // Assert
+            if (range == 0)
+            {
+                Assert.That(result, Is.Null);
+            }
+            else
+            {
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(Node<int>.Value)).EqualTo(0));
+                Assert.That(
+                    _list.GetAt(0),
+                    Is.Not.Null
+                    .And.Property(nameof(Node<int>.Value)).Not.EqualTo(0));
+            }
+            Assert.That(_list.Size, Is.EqualTo(Math.Max(0, previousSize - 1)));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void RemoveLast_WhenCalled_ShouldRemoveNodeFromTailAndDecrementSize(int range)
+        {
+            // Arrange
+            for (var i = 0; i < range; i++)
+            {
+                _list.AddLast(i);
+            }
+            var previousSize = _list.Size;
+
+            // Act
+            var result = _list.RemoveLast();
+
+            // Assert
+            if (range == 0)
+            {
+                Assert.That(result, Is.Null);
+            }
+            else
+            {
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(Node<int>.Value)).EqualTo(range - 1));
+                Assert.That(
+                    _list.GetAt(_list.Size - 1),
+                    Is.Not.Null
+                    .And.Property(nameof(Node<int>.Value)).Not.EqualTo(range - 1)
+                    .And.Property(nameof(Node<int>.Next)).Null);
+            }
+            Assert.That(_list.Size, Is.EqualTo(Math.Max(0, previousSize - 1)));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Reverse_WhenCalled_ShouldReverseNodes(int range)
+        {
+            // Arrange
+            for (var i = 0; i < range; i++)
+            {
+                _list.AddLast(i);
+            }
+
+            // Act
+            _list.Reverse();
+
+            // Assert
+            for (var i = 0; i < range; i++)
+            {
+                var result = _list.GetAt(i);
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(Node<int>.Value)).EqualTo(range - 1 - i));
+            }
+        }
+    }
+}
---
+++ DataStructures/Lists/SinglyLinkedList.cs
@@ -0,0 +1,134 @@
+﻿namespace DataStructures.Lists
+{
+    using System;
+
+    // Singly linked list without tail pointer
+    public class SinglyLinkedList<T>
+    {
+        public int Size { get; private set; }
+        public Node<T> Head { get; private set; }
+
+        // Get a node at the specified index
+        public Node<T> GetAt(int index)
+        {
+            if (index < 0 || index >= Size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var counter = 0;
+            var cur = Head;
+            while (counter != index)
+            {
+                cur = cur.Next;
+                counter++;
+            }
+
+            return cur;
+        }
+
+        // Set a node at the specified index
+        public void SetAt(int index, T value)
+        {
+            var node = GetAt(index);
+            node.Value = value;
+        }
+
+        // Add a node to the specified index
+        public void AddAt(int index, T value)
+        {
+            if (index == 0)
+            {
+                var head = new Node<T>(value, Head);
+                Head = head;
+                Size++;
+                return;
+            }
+
+            var node = GetAt(index - 1);
+            var temp = node.Next;
+            node.Next = new Node<T>(value, temp);
+            Size++;
+        }
+
+        // Add a node to the head of the list
+        public void AddFirst(T value)
+        {
+            AddAt(0, value);
+        }
+
+        // Add a node to the tail of the list
+        public void AddLast(T value)
+        {
+            AddAt(Size, value);
+        }
+
+        // Remove a node from the specified index
+        public Node<T> RemoveAt(int index)
+        {
+            if (index == 0)
+            {
+                var head = Head;
+                Head = head?.Next;
+                Size = Math.Max(0, Size - 1);
+                return head;
+            }
+
+            var node = GetAt(index - 1);
+            var temp = node.Next;
+            node.Next = temp?.Next;
+            Size--;
+            return temp;
+        }
+
+        // Remove a node from the head of the list
+        public Node<T> RemoveFirst()
+        {
+            return RemoveAt(0);
+        }
+
+        // Remove a node from the tail of the list
+        public Node<T> RemoveLast()
+        {
+            return RemoveAt(Math.Max(0, Size - 1));
+        }
+
+        // Reverse nodes
+        public void Reverse()
+        {
+            Node<T> prev = null;
+            var cur = Head;
+            while (cur != null)
+            {
+                var temp = cur.Next;
+                cur.Next = prev;
+                prev = cur;
+                cur = temp;
+
+                if (cur == null)
+                {
+                    Head = prev;
+                }
+            }
+        }
+
+        public class Node<NodeT>
+        {
+            public Node(NodeT value)
+            {
+                Value = value;
+            }
+
+            public Node(
+                NodeT value,
+                Node<NodeT> next)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public NodeT Value { get; set; }
+            public Node<NodeT> Next { get; set; }
+        }
+    }
+}
