6fb9993c818fbf788b5ad900913b04d3cf03292a
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-30 00:17:24 +0100

    Add stack with linked list

---
+++ DataStructures.Tests/Stacks/StackWithLinkedListTests.cs
@@ -0,0 +1,159 @@
+﻿namespace DataStructures.Tests.Stacks
+{
+    using System;
+    using DataStructures.Stacks;
+    using NUnit.Framework;
+
+    public class StackWithLinkedListTests
+    {
+        private StackWithLinkedList<int> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new StackWithLinkedList<int>();
+        }
+
+        [Test]
+        public void IsEmpty_WhenStackIsEmpty_ShouldReturnTrue()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_stack.IsEmpty, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsEmpty_WhenStackIsNotEmpty_ShouldReturnFalse()
+        {
+            // Arrange
+            _stack.Push(1);
+
+            // Act
+            var result = _stack.IsEmpty;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Count_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Act
+            var result = _stack.Count;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Clear_WhenCalled_ShouldClearStack(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Act
+            _stack.Clear();
+
+            // Assert
+            Assert.That(_stack.IsEmpty, Is.EqualTo(true));
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Peek_WhenStackIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _stack.Peek());
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Peek_WhenStackIsNotEmpty_ShouldReturnItemFromTopOfStack(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Act
+            var result = _stack.Peek();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        public void Pop_WhenStackIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _stack.Pop());
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Pop_WhenStackIsNotEmpty_ShouldRemoveItemFromTopOfStack(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+            var prevSize = _stack.Count;
+
+            // Act
+            var result = _stack.Pop();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+            Assert.That(_stack.Count, Is.EqualTo(prevSize - 1));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Push_WhenStackIsNotFull_ShouldAddItemToTopOfStack(int range)
+        {
+            // Arrange & Act
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Assert
+            Assert.That(_stack.Peek(), Is.EqualTo(range));
+            Assert.That(_stack.Count, Is.EqualTo(range));
+        }
+    }
+}
---
+++ DataStructures/Stacks/StackWithLinkedList.cs
@@ -0,0 +1,84 @@
+﻿namespace DataStructures.Stacks
+{
+    using System;
+
+    // Stack implemented using linked list
+    public class StackWithLinkedList<T>
+    {
+        private Node<T> _head;
+
+        // Represent whether a stack is empty or not
+        public bool IsEmpty
+        {
+            get
+            {
+                return Count == 0;
+            }
+        }
+
+        // Represent the number of items stored in a stack
+        public int Count { get; private set; }
+
+        // Clear a stack (and free memory) by letting GC take charge
+        public void Clear()
+        {
+            var cur = _head;
+            while (cur != null)
+            {
+                var next = cur.Next;
+                cur = null;
+                cur = next;
+            }
+
+            _head = null;
+            Count = 0;
+        }
+
+        // Return an item from the top of the stack
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            return _head.Value;
+        }
+
+        // Remove an item (and free memory) from the top of the stack
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            var item = _head;
+            _head = item.Next;
+            Count--;
+            return item.Value;
+        }
+
+        // Add an item to the top of the stack
+        public void Push(T value)
+        {
+            var node = new Node<T>(value, _head);
+            Count++;
+            _head = node;
+        }
+
+        public class Node<NodeT>
+        {
+            public Node(
+                NodeT value,
+                Node<NodeT> next = null)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public NodeT Value { get; set; }
+            public Node<NodeT> Next { get; set; }
+        }
+    }
+}
