a2cf71f37312be05b77798a94d932e376fcfe123
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-31 01:27:08 +0100

    Add queue with linked list

---
+++ DataStructures.Tests/Queues/QueueWithLinkedListTests.cs
@@ -0,0 +1,159 @@
+﻿namespace DataStructures.Tests.Queues
+{
+    using System;
+    using DataStructures.Queues;
+    using NUnit.Framework;
+
+    public class QueueWithLinkedListTests
+    {
+        private QueueWithLinkedList<int> _queue;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _queue = new QueueWithLinkedList<int>();
+        }
+
+        [Test]
+        public void IsEmpty_WhenQueueIsEmpty_ShouldReturnTrue()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_queue.IsEmpty, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsEmpty_WhenQueueIsNotEmpty_ShouldReturnFalse()
+        {
+            // Arrange
+            _queue.Enqueue(1);
+
+            // Act
+            var result = _queue.IsEmpty;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Count_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i);
+            }
+
+            // Act
+            var result = _queue.Count;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Clear_WhenCalled_ShouldClearQueue(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i);
+            }
+
+            // Act
+            _queue.Clear();
+
+            // Assert
+            Assert.That(_queue.IsEmpty, Is.EqualTo(true));
+            Assert.That(_queue.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Dequeue_WhenQueueIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _queue.Dequeue());
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Dequeue_WhenQueueIsNotEmpty_ShouldRemoveItemFromFrontOfQueue(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i);
+            }
+            var prevSize = _queue.Count;
+
+            // Act
+            var result = _queue.Dequeue();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(1));
+            Assert.That(_queue.Count, Is.EqualTo(prevSize - 1));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Enqueue_WhenCalled_ShouldAddItemToBackOfQueue(int range)
+        {
+            // Arrange & Act
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i);
+            }
+
+            // Assert
+            Assert.That(_queue.Peek(), Is.EqualTo(1));
+            Assert.That(_queue.Count, Is.EqualTo(range));
+        }
+
+        [Test]
+        public void Peek_WhenQueueIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _queue.Peek());
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Peek_WhenQueueIsNotEmpty_ShouldReturnItemFromFrontOfQueue(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i);
+            }
+
+            // Act
+            var result = _queue.Peek();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
---
+++ DataStructures/Queues/QueueWithLinkedList.cs
@@ -0,0 +1,98 @@
+﻿namespace DataStructures.Queues
+{
+    using System;
+
+    public class QueueWithLinkedList<T>
+    {
+        private Node<T> _front;
+        private Node<T> _back;
+        private int _size;
+
+        // Represent whether a queue is empty or not
+        public bool IsEmpty
+        {
+            get
+            {
+                return _size == 0;
+            }
+        }
+
+        // Represent the number of items stored in a queue
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        // Clear a queue (and free memory) by letting GC take charge
+        public void Clear()
+        {
+            var cur = _front;
+            while (cur != null)
+            {
+                var next = cur.Next;
+                cur = null;
+                cur = next;
+            }
+            _front = _back = null;
+            _size = 0;
+        }
+
+        // Remove an item from the front of the queue
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            var item = _front;
+            _front = _front.Next;
+            _size--;
+            return item.Value;
+        }
+
+        // Add an item to the back of the queue
+        public void Enqueue(T value)
+        {
+            var item = new Node<T>(value);
+            if (IsEmpty)
+            {
+                _front = item;
+            }
+            else
+            {
+                _back.Next = item;
+            }
+            _back = item;
+            _size++;
+        }
+
+        // Return an item from the front of the queue
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            return _front.Value;
+        }
+
+        public class Node<NodeT>
+        {
+            public Node(
+                NodeT value,
+                Node<NodeT> next = null)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public NodeT Value { get; set; }
+            public Node<NodeT> Next { get; set; }
+        }
+    }
+}
