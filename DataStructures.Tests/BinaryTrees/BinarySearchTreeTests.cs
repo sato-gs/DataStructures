1811730451cb7fea01f9227238b2d07e5f622d80
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-08-10 23:55:07 +0100

    Add binary search tree

---
+++ DataStructures.Tests/BinaryTrees/BinarySearchTreeTests.cs
@@ -0,0 +1,286 @@
+﻿namespace DataStructures.Tests.BinaryTrees
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataStructures.BinaryTrees;
+    using NUnit.Framework;
+
+    public class BinarySearchTreeTests
+    {
+        private BinarySearchTree<int> _tree;
+        private readonly List<int> _values = new List<int>() { 5, 3, 7, 2, 4, 6, 9, 1, 8, 10 };
+        /*
+                        5
+                     /     \ 
+                    3       7
+                   /  \    /  \ 
+                  2    4  6    9
+                 /            / \
+                1            8   10
+         */
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tree = new BinarySearchTree<int>();
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Find_WhenNodeDoesNotExist_ShouldReturnNull(int value)
+        {
+            // Arrange & Act
+            var result = _tree.Find(value);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Find_WhenNodeExists_ShouldReturnNode(int value)
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.Find(value);
+
+            // Assert
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                .EqualTo(value));
+        }
+
+        [Test]
+        public void FindMin_WhenNodesDoNotExist_ShouldReturnNull()
+        {
+            // Arrange & Act
+            var result = _tree.FindMin();
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void FindMin_WhenNodesExist_ShouldReturnMinNode()
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.FindMin();
+
+            // Assert
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                .EqualTo(1));
+        }
+
+        [Test]
+        public void FindMax_WhenNodesDoNotExist_ShouldReturnNull()
+        {
+            // Arrange & Act
+            var result = _tree.FindMax();
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void FindMax_WhenNodesExist_ShouldReturnMaxNode()
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.FindMax();
+
+            // Assert
+            Assert.That(
+                result,
+                Is.Not.Null
+                .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                .EqualTo(10));
+        }
+
+        [Test]
+        public void Insert_WhenNodeDoesNotExist_ShouldInsertNode()
+        {
+            // Arrange & Act
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Assert
+            _values.ForEach(val =>
+            {
+                var result = _tree.Find(val);
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                    .EqualTo(val));
+            });
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Insert_WhenNodeExists_ShouldIgnore(int value)
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            _tree.Insert(value);
+
+            // Assert
+            _values.ForEach(val =>
+            {
+                var result = _tree.Find(val);
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                    .EqualTo(val));
+            });
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(50)]
+        [TestCase(100)]
+        public void Delete_WhenNodeDoesNotExist_ShouldIgnore(int value)
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            _tree.Delete(value);
+
+            // Assert
+            _values.ForEach(val =>
+            {
+                var result = _tree.Find(val);
+                Assert.That(
+                    result,
+                    Is.Not.Null
+                    .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                    .EqualTo(val));
+            });
+        }
+
+        [Test]
+        [TestCase(1, new[] { 5, 3, 7, 2, 4, 6, 9, 8, 10 })]
+        [TestCase(5, new[] { 4, 3, 7, 2, 6, 9, 1, 8, 10 })]
+        [TestCase(10, new[] { 5, 3, 7, 2, 4, 6, 9, 1, 8 })]
+        public void Delete_WhenNodeExists_ShouldDeleteNode(int value, int[] expected)
+        {
+            // Arrange
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            _tree.Delete(value);
+
+            // Assert
+            _values.ForEach(val =>
+            {
+                var result = _tree.Find(val);
+                if (val == value)
+                {
+                    Assert.That(
+                        result,
+                        Is.Null);
+                }
+                else
+                {
+                    Assert.That(
+                        result,
+                        Is.Not.Null
+                        .And.Property(nameof(BinarySearchTreeWithDuplicate<int>.Node<int>.Value))
+                        .EqualTo(val));
+                }
+            });
+            foreach (var (val, index) in _tree.LevelOrderTraverse().Select((val, index) => (val, index)))
+            {
+                Assert.That(expected[index], Is.EqualTo(val));
+            }
+        }
+
+        [Test]
+        public void LevelOrderTraverse_WhenCalled_ShouldTraverseViaLevelOrderTraversal()
+        {
+            // Arrange
+            var expected = new int[] { 5, 3, 7, 2, 4, 6, 9, 1, 8, 10 };
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.LevelOrderTraverse();
+
+            // Assert
+            foreach (var (val, index) in result.Select((val, index) => (val, index)))
+            {
+                Assert.That(expected[index], Is.EqualTo(val));
+            }
+        }
+
+        [Test]
+        public void PreOrderTraverse_WhenCalled_ShouldTraverseViaPreOrderTraversal()
+        {
+            // Arrange
+            var expected = new int[] { 5, 3, 2, 1, 4, 7, 6, 9, 8, 10 };
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.PreOrderTraverse();
+
+            // Assert
+            foreach (var (val, index) in result.Select((val, index) => (val, index)))
+            {
+                Assert.That(expected[index], Is.EqualTo(val));
+            }
+        }
+
+        [Test]
+        public void PostOrderTraverse_WhenCalled_ShouldTraverseViaPostOrderTraversal()
+        {
+            // Arrange
+            var expected = new int[] { 1, 2, 4, 3, 6, 8, 10, 9, 7, 5 };
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.PostOrderTraverse();
+
+            // Assert
+            foreach (var (val, index) in result.Select((val, index) => (val, index)))
+            {
+                Assert.That(expected[index], Is.EqualTo(val));
+            }
+        }
+
+        [Test]
+        public void InOrderTraverse_WhenCalled_ShouldTraverseViaInOrderTraversal()
+        {
+            // Arrange
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            _values.ForEach(val => _tree.Insert(val));
+
+            // Act
+            var result = _tree.InOrderTraverse();
+
+            // Assert
+            foreach (var (val, index) in result.Select((val, index) => (val, index)))
+            {
+                Assert.That(expected[index], Is.EqualTo(val));
+            }
+        }
+    }
+}
---
+++ DataStructures/BinaryTrees/BinarySearchTree.cs
@@ -0,0 +1,326 @@
+﻿namespace DataStructures.BinaryTrees
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    // Binary search tree with no duplicate allowed
+    public class BinarySearchTree<T>
+    {
+        // Represent the root node
+        public Node<T> Root { get; private set; }
+
+        // Find a node with a given value
+        public Node<T> Find(T value)
+        {
+            return Find(Root, value);
+        }
+
+        // Find a node with a given value recursively
+        private Node<T> Find(Node<T> node, T value)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            var cmp = Comparer<T>
+                .Default
+                .Compare(value, node.Value);
+            // If a value is less than the value of the current node
+            // Continue with its left sub-tree
+            if (cmp < 0)
+            {
+                return Find(node.Left, value);
+            }
+            // If a value is greater than the value of the current node
+            // Continue with its right sub-tree
+            else if (cmp > 0)
+            {
+                return Find(node.Right, value);
+            }
+            // If a value is equal to the value of the current node
+            // Return it
+            else
+            {
+                return node;
+            }
+        }
+
+        // Find a node with a minimum value
+        public Node<T> FindMin()
+        {
+            return FindMin(Root);
+        }
+
+        // Find a node with a minimum value recursively
+        private Node<T> FindMin(Node<T> node)
+        {
+            if (node?.Left == null)
+            {
+                return node;
+            }
+            else
+            {
+                return FindMin(node.Left);
+            }
+        }
+
+        // Find a node with a maximum value
+        public Node<T> FindMax()
+        {
+            return FindMax(Root);
+        }
+
+        // Find a node with a maximum value recursively
+        private Node<T> FindMax(Node<T> node)
+        {
+            if (node?.Right == null)
+            {
+                return node;
+            }
+            else
+            {
+                return FindMax(node.Right);
+            }
+        }
+
+        // Insert a node with a given value
+        public void Insert(T value)
+        {
+            Root = Insert(Root, value);
+        }
+
+        // Insert a node with a given value recursively
+        private Node<T> Insert(Node<T> node, T value)
+        {
+            if (node == null)
+            {
+                return new Node<T>(value);
+            }
+
+            var cmp = Comparer<T>
+                .Default
+                .Compare(value, node.Value);
+            // If a value is less than the value of the current node
+            // Continue with its left sub-tree
+            if (cmp < 0)
+            {
+                node.Left = Insert(node.Left, value);
+            }
+            // If a value is greater than the value of the current node
+            // Continue with its right sub-tree
+            else if (cmp > 0)
+            {
+                node.Right = Insert(node.Right, value);
+            }
+            // If a value is equal to the value of the current node
+            // Just ignore
+
+            return node;
+        }
+
+        // Delete a node with a given value
+        public void Delete(T value)
+        {
+            Root = Delete(Root, value);
+        }
+
+        // Delete a node with a given value recursively
+        private Node<T> Delete(Node<T> node, T value)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            var cmp = Comparer<T>
+                .Default
+                .Compare(value, node.Value);
+            // If a value is less than the value of the current node
+            // Continue with its left sub-tree
+            if (cmp < 0)
+            {
+                node.Left = Delete(node.Left, value);
+            }
+            // If a value is greater than the value of the current node
+            // Continue with its right sub-tree
+            else if (cmp > 0)
+            {
+                node.Right = Delete(node.Right, value);
+            }
+            // If a value is equal to the value of the current node
+            // Delete a node
+            else
+            {
+                // If the current node has no left node
+                // Delete the current node and return its right node (as its replacement)
+                if (node.Left == null)
+                {
+                    var rightNode = node.Right;
+
+                    node.Value = default;
+                    node = null;
+
+                    return rightNode;
+                }
+                // If the current node has no right node
+                // Delete the current node and return its left node (as its replacement)
+                else if (node.Right == null)
+                {
+                    var leftNode = node.Left;
+
+                    node.Value = default;
+                    node = null;
+
+                    return leftNode;
+                }
+                // If the current node has two child nodes
+                // Replace it with a successor to maintain a binary search tree structure
+                // The successor can be either (1) the largest value in its left sub-tree or (2) the smallest value in its right sub-tree
+                else
+                {
+                    // Find the largest value in its left sub-tree as its successor
+                    var successor = FindMax(node.Left);
+
+                    // Swap its value
+                    node.Value = successor.Value;
+
+                    // Delete the successor
+                    node.Left = Delete(node.Left, successor.Value);
+                }
+            }
+
+            return node;
+        }
+
+        // Traverse via level order traversal (BFS)
+        public List<T> LevelOrderTraverse()
+        {
+            var list = new List<T>();
+            if (Root == null)
+            {
+                return list;
+            }
+
+            // Define a queue
+            var queue = new Queue<Node<T>>();
+            // Add a root node to the queue
+            queue.Enqueue(Root);
+
+            // Continue until the queue is empty
+            while (queue.Count != 0)
+            {
+                // Get a node at the beginning of the queue (e.g. Dequeue)
+                var node = queue.Dequeue();
+                // Do something with a current node as required
+                DoSomething(node, list);
+
+                // If there is a left node
+                if (node.Left != null)
+                {
+                    // Add it to the back of the queue (e.g. Enqueue) first
+                    queue.Enqueue(node.Left);
+                }
+
+                // If there is a right node
+                if (node.Right != null)
+                {
+                    // Add it to the back of the queue (e.g. Enqueue) second
+                    queue.Enqueue(node.Right);
+                }
+            }
+
+            return list;
+        }
+
+        // Traverse via pre-order traversal (DFS)
+        public List<T> PreOrderTraverse()
+        {
+            var list = new List<T>();
+            // Define a recursive function
+            void Traverse(Node<T> node)
+            {
+                if (node != null)
+                {
+                    // Do something with a current node as required first
+                    DoSomething(node, list);
+                    // Call itself with a left node of the current node second
+                    Traverse(node.Left);
+                    // Call itself with a right node of the current node third
+                    Traverse(node.Right);
+                }
+            }
+
+            // Call the recursive function with a root node
+            Traverse(Root);
+            return list;
+        }
+
+        // Traverse via post-order traversal (DFS)
+        public List<T> PostOrderTraverse()
+        {
+            var list = new List<T>();
+            // Define a recursive function
+            void Traverse(Node<T> node)
+            {
+                if (node != null)
+                {
+                    // Call itself with a left node of the current node first
+                    Traverse(node.Left);
+                    // Call itself with a right node of the current node second
+                    Traverse(node.Right);
+                    // Do something with a current node as required third
+                    DoSomething(node, list);
+                }
+            }
+
+            // Call the recursive function with a root node
+            Traverse(Root);
+            return list;
+        }
+
+        // Traverse via in-order traversal (DFS)
+        public List<T> InOrderTraverse()
+        {
+            var list = new List<T>();
+            // Define a recursive function
+            void Traverse(Node<T> node)
+            {
+                if (node != null)
+                {
+                    // Call itself with a left node of the current node first
+                    Traverse(node.Left);
+                    // Do something with a current node as required second
+                    DoSomething(node, list);
+                    // Call itself with a right node of the current node third
+                    Traverse(node.Right);
+                }
+            }
+
+            // Call the recursive function with a root node
+            Traverse(Root);
+            return list;
+        }
+
+        private void DoSomething(Node<T> node, List<T> list = null)
+        {
+            Trace.WriteLine(node.Value);
+            if (list != null)
+            {
+                list.Add(node.Value);
+            }
+        }
+
+        public class Node<NodeT>
+        {
+            public NodeT Value { get; set; }
+            public Node<NodeT> Left { get; set; }
+            public Node<NodeT> Right { get; set; }
+
+            public Node(NodeT value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
