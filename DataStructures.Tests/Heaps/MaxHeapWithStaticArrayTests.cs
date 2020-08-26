534c83e60e83d49717e65a6a77c76a58eaf56a6b
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-08-27 00:28:31 +0100

    Add max heap

---
+++ DataStructures.Tests/Heaps/MaxHeapWithStaticArrayTests.cs
@@ -0,0 +1,324 @@
+﻿namespace DataStructures.Tests.Heaps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataStructures.Heaps;
+    using NUnit.Framework;
+
+    public class MaxHeapWithStaticArrayTests
+    {
+        private MaxHeapWithStaticArray<int> _heap;
+        private readonly int _capacity = 10;
+        private readonly int[] _values = new int[] { 5, 3, 7, 2, 4, 6, 9, 1, 8, 10 };
+
+        private readonly Dictionary<int, int[]> _answersForInsertion = new Dictionary<int, int[]>()
+        {
+
+            { 1, new int[] { 5 } },
+            /* (1)
+			            5
+	        */
+            { 2, new int[] { 5, 3 } },
+            /* (2)
+			            5
+			         /
+			        3
+	        */
+            { 3, new int[] { 7, 3, 5 } },
+            /* (3)
+			            7
+			         /     \ 
+			        3       5
+	        */
+            { 4, new int[] { 7, 3, 5, 2 } },
+            /* (4)
+			            7
+			         /     \ 
+			        3       5
+                   /
+                  2
+	        */
+            { 5, new int[] { 7, 4, 5, 2, 3 } },
+            /* (5)
+			            7
+			         /     \ 
+			        4       5
+                   /  \
+                  2    3
+	        */
+            { 6, new int[] { 7, 4, 6, 2, 3, 5 } },
+            /* (6)
+			            7
+			         /     \ 
+			        4       6
+                   /  \    /
+                  2    3  5
+	        */
+            { 7, new int[] { 9, 4, 7, 2, 3, 5, 6 } },
+            /* (7)
+			            9
+			         /     \ 
+			        4       7
+                   /  \    /  \
+                  2    3  5    6
+	        */
+            { 8, new int[] { 9, 4, 7, 2, 3, 5, 6, 1 } },
+            /* (8)
+			            9
+			         /     \ 
+			        4       7
+                   /  \    /  \
+                  2    3  5    6
+                 /
+                1
+	        */
+            { 9, new int[] { 9, 8, 7, 4, 3, 5, 6, 1, 2 } },
+            /* (9)
+			            9
+			         /     \ 
+			        8       7
+                   /  \    /  \
+                  4    3  5    6
+                 /  \
+                1    2
+	        */
+            { 10, new int[] { 10, 9, 7, 4, 8, 5, 6, 1, 2, 3 } }
+            /* (10)
+			            10
+			         /     \ 
+			        9       7
+                   /  \    /  \
+                  4    8  5    6
+                 /  \  |
+                1    2 3
+	        */
+        };
+
+        private readonly Dictionary<int, int[]> _answersForDeletion = new Dictionary<int, int[]>()
+        {
+            { 1, new int[] { 9, 8, 7, 4, 3, 5, 6, 1, 2 } },
+            /* (1)
+			            9
+			         /     \ 
+			        8       7
+                   /  \    /  \
+                  4    3  5    6
+                 /  \
+                1    2
+	        */
+            { 2, new int[] { 8, 4, 7, 2, 3, 5, 6, 1 } },
+            /* (2)
+			            8
+			         /     \ 
+			        4       7
+                   /  \    /  \
+                  2    3  5    6
+                 /
+                1
+	        */
+            { 3, new int[] { 7, 4, 6, 2, 3, 5, 1 } },
+            /* (3)
+			            7
+			         /     \ 
+			        4       6
+                   /  \    /  \
+                  2    3  5    1
+	        */
+            { 4, new int[] { 6, 4, 5, 2, 3, 1 } },
+            /* (4)
+			            6
+			         /     \ 
+			        4       5
+                   /  \    /
+                  2    3  1
+	        */
+            { 5, new int[] { 5, 4, 1, 2, 3 } },
+            /* (5)
+			            5
+			         /     \ 
+			        4       1
+                   /  \
+                  2    3
+	        */
+            { 6, new int[] { 4, 3, 1, 2 } },
+            /* (6)
+			            4
+			         /     \ 
+			        3       1
+                   /
+                  2
+	        */
+            { 7, new int[] { 3, 2, 1 } },
+            /* (7)
+			            3
+			         /     \ 
+			        2       1
+	        */
+            { 8, new int[] { 2, 1 } },
+            /* (8)
+			            2
+			         /
+			        1
+	        */
+            { 9, new int[] { 1 } },
+            /* (9)
+			            1
+	        */
+            { 10, new int[] { } }
+            /* (10)
+
+	        */
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            _heap = new MaxHeapWithStaticArray<int>(_capacity);
+        }
+
+        [Test]
+        public void IsEmpty_WhenHeapIsEmpty_ShouldReturnTrue()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_heap.IsEmpty, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsEmpty_WhenHeapIsNotEmpty_ShouldReturnFalse()
+        {
+            // Arrange
+            _heap.Insert(1);
+
+            // Act
+            var result = _heap.IsEmpty;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsFull_WhenHeapIsFull_ShouldReturnTrue()
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _heap.Insert(i);
+            }
+
+            // Act
+            var result = _heap.IsFull;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsFull_WhenHeapIsNotFull_ShouldReturnFalse()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_heap.IsFull, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Count_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _heap.Insert(i);
+            }
+
+            // Act
+            var result = _heap.Count;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        public void Insert_WhenHeapIsFull_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _heap.Insert(i);
+            }
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _heap.Insert(100));
+        }
+
+        [Test]
+        public void Insert_WhenHeapIsNotFull_ShouldInsertItemWithMaxHeapPropertyMaintained()
+        {
+            // Arrange & Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _heap.Insert(_values[i]);
+                var answer = _answersForInsertion[i + 1];
+                Assert.That(_heap.PeekMax(), Is.EqualTo(answer.Max()));
+                Assert.That(_heap.Count, Is.EqualTo(i + 1));
+                for (var j = 0; j < answer.Length; j++)
+                {
+                    Assert.That(_heap.GetAt(j), Is.EqualTo(answer[j]));
+                }
+            }
+        }
+
+        [Test]
+        public void PeekMax_WhenHeapIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _heap.PeekMax());
+        }
+
+        [Test]
+        public void PeekMax_WhenHeapIsNotEmpty_ShouldReturnMaximumItem()
+        {
+            // Arrange & Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _heap.Insert(_values[i]);
+                var answer = _answersForInsertion[i + 1];
+                Assert.That(_heap.PeekMax(), Is.EqualTo(answer.Max()));
+                Assert.That(_heap.Count, Is.EqualTo(i + 1));
+            }
+        }
+
+        [Test]
+        public void PopMax_WhenHeapIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _heap.PopMax());
+        }
+
+        [Test]
+        public void PopMax_WhenHeapIsNotEmpty_ShouldRemoveMaximumItemWithMaxHeapPropertyMaintained()
+        {
+            // Arrange
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _heap.Insert(_values[i]);
+            }
+
+            // Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                var answer = _answersForDeletion[i + 1];
+                Assert.That(_heap.PopMax(), Is.EqualTo(_values.Length - i));
+                Assert.That(_heap.Count, Is.EqualTo(_values.Length - i - 1));
+                for (var j = 0; j < answer.Length; j++)
+                {
+                    Assert.That(_heap.GetAt(j), Is.EqualTo(answer[j]));
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Heaps/MaxHeapWithStaticArray.cs
@@ -0,0 +1,186 @@
+﻿namespace DataStructures.Heaps
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeapWithStaticArray<T>
+    {
+        // Represent the heap
+        private readonly T[] _heap;
+        // Represent the current size of the heap
+        private int _size;
+
+        // Represent whether the heap is empty or not
+        public bool IsEmpty
+        {
+            get
+            {
+                return _size == 0;
+            }
+        }
+
+        // Represent whether the heap is full or not
+        public bool IsFull
+        {
+            get
+            {
+                return _size == _heap.Length;
+            }
+        }
+
+        // Represent the number of items stored in the heap
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public MaxHeapWithStaticArray(int capacity)
+        {
+            _heap = new T[capacity];
+            _size = 0;
+        }
+
+        // Insert an item while maintaining the max heap property
+        public void Insert(T value)
+        {
+            if (IsFull)
+            {
+                throw new InvalidOperationException("The heap is full.");
+            }
+
+            // Add an item to the end of the heap
+            _heap[_size] = value;
+            _size++;
+            // Restructure the heap to maintain the max heap property
+            HeapifyUp(_size - 1);
+        }
+
+        // Return a maximum item
+        public T PeekMax()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The heap is empty.");
+            }
+
+            return _heap[0];
+        }
+
+        // Remove a maximum item while maintaining the max heap property
+        public T PopMax()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The heap is empty.");
+            }
+
+            // Remove a maximum item from the top of the heap and replace it with the last item
+            var max = _heap[0];
+            _heap[0] = _heap[_size - 1];
+            _heap[_size - 1] = default;
+            _size--;
+            // Restructure the heap to maintain the max heap property
+            HeapifyDown(0);
+
+            return max;
+        }
+
+        // Return an item stored at a given index (as a test helper function)
+        public T GetAt(int index)
+        {
+            return _heap[index];
+        }
+
+        // Return the index of the parent of an item stored at a given index
+        private int GetParentIndex(int index) => (index - 1) / 2;
+
+        // Return the index of the left child of an item stored at a given index
+        private int GetLeftChildIndex(int index) => index * 2 + 1;
+
+        // Return the index of the right child of an item stored at a given index
+        private int GetRightChildIndex(int index) => index * 2 + 2;
+
+        // Return the left child of an item stored at a given index
+        private T GetLeftChild(int index) => _heap[GetLeftChildIndex(index)];
+
+        // Return the right child of an item stored at a given index
+        private T GetRightChild(int index) => _heap[GetRightChildIndex(index)];
+
+        // Check whether an item stored at a given index has a left child or not
+        private bool HasLeftChild(int index) => GetLeftChildIndex(index) < _size;
+
+        // Check whether an item stored at a given index has a right child or not
+        private bool HasRightChild(int index) => GetRightChildIndex(index) < _size;
+
+        // Restructure the heap bottom-up in such ways that the max heap property is maintained
+        private void HeapifyUp(int index)
+        {
+            // If it is a root item
+            // Break out of the recursion
+            if (index == 0)
+            {
+                return;
+            }
+
+            var cur = _heap[index];
+            var parentIndex = GetParentIndex(index);
+
+            // If a current child is equal to or smaller than its parent
+            // Break out of the recursion
+            var cmp = Comparer<T>
+                .Default
+                .Compare(cur, _heap[parentIndex]);
+            if (cmp <= 0)
+            {
+                return;
+            }
+
+            // Otherwise, swap them and continue recursively
+            Swap(index, parentIndex);
+            HeapifyUp(parentIndex);
+        }
+
+        // Restructure the heap top-down in such ways that the max heap property is maintained
+        private void HeapifyDown(int index)
+        {
+            // If there is no left child (meaning no child)
+            // Break out of the recursion
+            if (!HasLeftChild(index))
+            {
+                return;
+            }
+
+            var largerChildIndex = GetLeftChildIndex(index);
+            if (HasRightChild(index)
+                && Comparer<T>.Default.Compare(GetLeftChild(index), GetRightChild(index)) < 0)
+            {
+                largerChildIndex = GetRightChildIndex(index);
+            }
+
+            // If a current parent is equal to or larger than its larger child
+            // Break out of the recursion
+            var cmp = Comparer<T>
+                .Default
+                .Compare(_heap[index], _heap[largerChildIndex]);
+            if (cmp >= 0)
+            {
+                return;
+            }
+
+            // Otherwise, swap them and continue recursively
+            Swap(index, largerChildIndex);
+            HeapifyDown(largerChildIndex);
+        }
+
+        // Swap two items
+        private void Swap(int index1, int index2)
+        {
+            var temp = _heap[index1];
+            _heap[index1] = _heap[index2];
+            _heap[index2] = temp;
+        }
+    }
+}
