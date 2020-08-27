a84de3dc88790d985a462b36965bed0fbddaaab8
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-08-27 19:59:19 +0100

    Add priority queue

---
+++ DataStructures.Tests/Heaps/PriorityQueueWithMinHeapTests.cs
@@ -0,0 +1,331 @@
+﻿namespace DataStructures.Tests.Heaps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataStructures.Heaps;
+    using NUnit.Framework;
+
+    public class PriorityQueueWithMinHeapTests
+    {
+        private PriorityQueueWithMinHeap<string> _queue;
+        private readonly int _capacity = 10;
+        private readonly int[] _values = new int[] { 5, 3, 7, 2, 4, 6, 9, 1, 8, 10 };
+
+        private readonly Dictionary<int, int[]> _answersForInsertion = new Dictionary<int, int[]>()
+        {
+
+            { 1, new int[] { 5 } },
+            /* (1)
+			            5
+	        */
+            { 2, new int[] { 3, 5 } },
+            /* (2)
+			            3
+			         /
+			        5
+	        */
+            { 3, new int[] { 3, 5, 7 } },
+            /* (3)
+			            3
+			         /     \ 
+			        5       7
+	        */
+            { 4, new int[] { 2, 3, 7, 5 } },
+            /* (4)
+			            2
+			         /     \ 
+			        3       7
+                   /
+                  5
+	        */
+            { 5, new int[] { 2, 3, 7, 5, 4 } },
+            /* (5)
+			            2
+			         /     \ 
+			        3       7
+                   /  \
+                  5    4
+	        */
+            { 6, new int[] { 2, 3, 6, 5, 4, 7 } },
+            /* (6)
+			            2
+			         /     \ 
+			        3       6
+                   /  \    /
+                  5    4  7
+	        */
+            { 7, new int[] { 2, 3, 6, 5, 4, 7, 9 } },
+            /* (7)
+			            2
+			         /     \ 
+			        3       6
+                   /  \    /  \
+                  5    4  7    9
+	        */
+            { 8, new int[] { 1, 2, 6, 3, 4, 7, 9, 5 } },
+            /* (8)
+			            1
+			         /     \ 
+			        2       6
+                   /  \    /  \
+                  3    4  7    9
+                 /
+                5
+	        */
+            { 9, new int[] { 1, 2, 6, 3, 4, 7, 9, 5, 8 } },
+            /* (9)
+			            1
+			         /     \ 
+			        2       6
+                   /  \    /  \
+                  3    4  7    9
+                 /  \
+                5    8
+	        */
+            { 10, new int[] { 1, 2, 6, 3, 4, 7, 9, 5, 8, 10 } }
+            /* (10)
+			            1
+			         /     \ 
+			        2       6
+                   /  \    /  \
+                  3    4  7    9
+                 /  \  |
+                5    8 10
+	        */
+        };
+
+        private readonly Dictionary<int, int[]> _answersForDeletion = new Dictionary<int, int[]>()
+        {
+            { 1, new int[] { 2, 3, 6, 5, 4, 7, 9, 10, 8 } },
+            /* (1)
+			            2
+			         /     \ 
+			        3       6
+                   /  \    /  \
+                  5    4  7    9
+                 /  \
+                10   8
+	        */
+            { 2, new int[] { 3, 4, 6, 5, 8, 7, 9, 10 } },
+            /* (2)
+			            3
+			         /     \ 
+			        4       6
+                   /  \    /  \
+                  5    8  7    9
+                 /
+                10
+	        */
+            { 3, new int[] { 4, 5, 6, 10, 8, 7, 9 } },
+            /* (3)
+			            4
+			         /     \ 
+			        5       6
+                   /  \    /  \
+                  10   8  7    9
+	        */
+            { 4, new int[] { 5, 8, 6, 10, 9, 7 } },
+            /* (4)
+			            5
+			         /     \ 
+			        8       6
+                   /  \    /
+                  10   9  7
+	        */
+            { 5, new int[] { 6, 8, 7, 10, 9 } },
+            /* (5)
+			            6
+			         /     \ 
+			        8       7
+                   /  \
+                  10   9
+	        */
+            { 6, new int[] { 7, 8, 9, 10 } },
+            /* (6)
+			            7
+			         /     \ 
+			        8       9
+                   /
+                  10
+	        */
+            { 7, new int[] { 8, 10, 9 } },
+            /* (7)
+			            8
+			         /     \ 
+			        10      9
+	        */
+            { 8, new int[] { 9, 10 } },
+            /* (8)
+			            9
+			         /
+			        10
+	        */
+            { 9, new int[] { 10 } },
+            /* (9)
+			            10
+	        */
+            { 10, new int[] { } }
+            /* (10)
+            
+	        */
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            _queue = new PriorityQueueWithMinHeap<string>(_capacity);
+        }
+
+        [Test]
+        public void IsEmpty_WhenPriorityQueueIsEmpty_ShouldReturnTrue()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_queue.IsEmpty, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsEmpty_WhenPriorityQueueIsNotEmpty_ShouldReturnFalse()
+        {
+            // Arrange
+            _queue.Enqueue(1, "Item 1");
+
+            // Act
+            var result = _queue.IsEmpty;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsFull_WhenPriorityQueueIsFull_ShouldReturnTrue()
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _queue.Enqueue(i, $"Item {i}");
+            }
+
+            // Act
+            var result = _queue.IsFull;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsFull_WhenPriorityQueueIsNotFull_ShouldReturnFalse()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_queue.IsFull, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Count_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _queue.Enqueue(i, $"Item {i}");
+            }
+
+            // Act
+            var result = _queue.Count;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        public void Enqueue_WhenCalled_ShouldAddItemWithPriorityPropertyMaintained()
+        {
+            // Arrange & Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _queue.Enqueue(_values[i], $"Item {_values[i]}");
+                var answer = _answersForInsertion[i + 1];
+                Assert.That(
+                    _queue.PeekPriority(),
+                    Is.Not.Null
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Priority)).EqualTo(answer.Min())
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Value)).EqualTo($"Item {answer.Min()}"));
+                Assert.That(_queue.Count, Is.EqualTo(i + 1));
+                for (var j = 0; j < answer.Length; j++)
+                {
+                    Assert.That(
+                        _queue.GetAt(j),
+                        Is.Not.Null
+                        .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Priority)).EqualTo(answer[j])
+                        .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Value)).EqualTo($"Item {answer[j]}"));
+                }
+            }
+        }
+
+        [Test]
+        public void PeekPriority_WhenPriorityQueueIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _queue.PeekPriority());
+        }
+
+        [Test]
+        public void PeekPriority_WhenPriorityQueueIsNotEmpty_ShouldReturnHighestPriorityItem()
+        {
+            // Arrange & Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _queue.Enqueue(_values[i], $"Item {_values[i]}");
+                var answer = _answersForInsertion[i + 1];
+                Assert.That(
+                    _queue.PeekPriority(),
+                    Is.Not.Null
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Priority)).EqualTo(answer.Min())
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Value)).EqualTo($"Item {answer.Min()}"));
+                Assert.That(_queue.Count, Is.EqualTo(i + 1));
+            }
+        }
+
+        [Test]
+        public void Dequeue_WhenPriorityQueueIsEmpty_ShouldThrowInvalidOperationException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _queue.Dequeue());
+        }
+
+        [Test]
+        public void Dequeue_WhenPriorityQueueIsNotEmpty_ShouldRemoveHighestPriorityItemWithPriorityPropertyMaintained()
+        {
+            // Arrange
+            for (var i = 0; i < _values.Length; i++)
+            {
+                _queue.Enqueue(_values[i], $"Item {_values[i]}");
+            }
+
+            // Act & Assert
+            for (var i = 0; i < _values.Length; i++)
+            {
+                var answer = _answersForDeletion[i + 1];
+                Assert.That(
+                    _queue.Dequeue(),
+                    Is.Not.Null
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Priority)).EqualTo(i + 1)
+                    .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Value)).EqualTo($"Item {i + 1}"));
+                Assert.That(_queue.Count, Is.EqualTo(_values.Length - i - 1));
+                for (var j = 0; j < answer.Length; j++)
+                {
+                    Assert.That(
+                        _queue.GetAt(j),
+                        Is.Not.Null
+                        .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Priority)).EqualTo(answer[j])
+                        .And.Property(nameof(PriorityQueueWithMinHeap<string>.Node<string>.Value)).EqualTo($"Item {answer[j]}"));
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Heaps/PriorityQueueWithMaxHeap.cs
@@ -0,0 +1,211 @@
+﻿namespace DataStructures.Heaps
+{
+    using System;
+
+    // Priority queue implemented using max heap (e.g. the larger the priority is, the higher the priority is)
+    public class PriorityQueueWithMaxHeap<T>
+    {
+        // Represent the priority queue
+        private Node<T>[] _queue;
+        // Represent the current size of the priority queue
+        private int _size;
+        // Represent the default capacity of the priority queue
+        private readonly int _defaultCapacity = 5;
+
+        // Represent whether the priority queue is empty or not
+        public bool IsEmpty
+        {
+            get
+            {
+                return _size == 0;
+            }
+        }
+
+        // Represent whether the priority queue is full or not
+        public bool IsFull
+        {
+            get
+            {
+                return _size == _queue.Length;
+            }
+        }
+
+        // Represent the number of items stored in the priority queue
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public PriorityQueueWithMaxHeap()
+        {
+            _queue = new Node<T>[_defaultCapacity];
+            _size = 0;
+        }
+
+        public PriorityQueueWithMaxHeap(int capacity)
+        {
+            _queue = new Node<T>[capacity];
+            _size = 0;
+        }
+
+        // Add an item while maintaining the priority property
+        public void Enqueue(
+            int priority,
+            T value)
+        {
+            if (IsFull)
+            {
+                Resize(Math.Max(_defaultCapacity, _queue.Length * 2));
+            }
+
+            // Add an item to the end of the priority queue
+            _queue[_size] = new Node<T>(priority, value);
+            _size++;
+            // Restructure the priority queue to maintain the priority property
+            RestructureUp(_size - 1);
+        }
+
+        // Return the highest priority item
+        public Node<T> PeekPriority()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The priority queue is empty.");
+            }
+
+            return _queue[0];
+        }
+
+        // Remove the highest priority item while maintaining the priority property
+        public Node<T> Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The priority queue is empty.");
+            }
+
+            // Remove the highest priority item from the top of the priority queue and replace it with the last item
+            var item = _queue[0];
+            _queue[0] = _queue[_size - 1];
+            _queue[_size - 1] = null;
+            _size--;
+            // Restructure the priority queue to maintain the priority property
+            RestructureDown(0);
+
+            return item;
+        }
+
+        // Return an item stored at a given index (as a test helper function)
+        public Node<T> GetAt(int index)
+        {
+            return _queue[index];
+        }
+
+        // Return the index of the parent of an item stored at a given index
+        private int GetParentIndex(int index) => (index - 1) / 2;
+
+        // Return the index of the left child of an item stored at a given index
+        private int GetLeftChildIndex(int index) => index * 2 + 1;
+
+        // Return the index of the right child of an item stored at a given index
+        private int GetRightChildIndex(int index) => index * 2 + 2;
+
+        // Return the left child of an item stored at a given index
+        private Node<T> GetLeftChild(int index) => _queue[GetLeftChildIndex(index)];
+
+        // Return the right child of an item stored at a given index
+        private Node<T> GetRightChild(int index) => _queue[GetRightChildIndex(index)];
+
+        // Check whether an item stored at a given index has a left child or not
+        private bool HasLeftChild(int index) => GetLeftChildIndex(index) < _size;
+
+        // Check whether an item stored at a given index has a right child or not
+        private bool HasRightChild(int index) => GetRightChildIndex(index) < _size;
+
+        // Restructure the priority queue bottom-up in such ways that the priority property is maintained
+        private void RestructureUp(int index)
+        {
+            // If it is a root item
+            // Break out of the recursion
+            if (index == 0)
+            {
+                return;
+            }
+
+            // If a current child is equal to or smaller than its parent in terms of priority
+            // Break out of the recursion
+            var cur = _queue[index];
+            var parentIndex = GetParentIndex(index);
+            if (cur.Priority <= _queue[parentIndex].Priority)
+            {
+                return;
+            }
+
+            // Otherwise, swap them and continue recursively
+            Swap(index, parentIndex);
+            RestructureUp(parentIndex);
+        }
+
+        // Restructure the priority queue top-down in such ways that the priority property is maintained
+        private void RestructureDown(int index)
+        {
+            // If there is no left child (meaning no child)
+            // Break out of the recursion
+            if (!HasLeftChild(index))
+            {
+                return;
+            }
+
+            var largerChildIndex = GetLeftChildIndex(index);
+            if (HasRightChild(index)
+                && GetLeftChild(index).Priority < GetRightChild(index).Priority)
+            {
+                largerChildIndex = GetRightChildIndex(index);
+            }
+
+            // If a current parent is equal to or larger than its larger child in terms of priority
+            // Break out of the recursion
+            if (_queue[index].Priority >= _queue[largerChildIndex].Priority)
+            {
+                return;
+            }
+
+            // Otherwise, swap them and continue recursively
+            Swap(index, largerChildIndex);
+            RestructureDown(largerChildIndex);
+        }
+
+        // Swap two items
+        private void Swap(int index1, int index2)
+        {
+            var temp = _queue[index1];
+            _queue[index1] = _queue[index2];
+            _queue[index2] = temp;
+        }
+
+        // Resize the priority queue
+        private void Resize(int size)
+        {
+            var array = new Node<T>[size];
+            Array.Copy(_queue, 0, array, 0, _size);
+            _queue = array;
+        }
+
+        public class Node<NodeT>
+        {
+            public int Priority { get; }
+            public NodeT Value { get; set; }
+
+            public Node(
+                int priority,
+                NodeT value)
+            {
+                Priority = priority;
+                Value = value;
+            }
+        }
+    }
+}
