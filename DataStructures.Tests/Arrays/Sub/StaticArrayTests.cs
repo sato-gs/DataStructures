1a9c30e349a2529656e8266024ac92405a6221f6
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-09-06 01:53:42 +0100

    Add static array

---
+++ DataStructures.Tests/Arrays/Sub/StaticArrayTests.cs
@@ -0,0 +1,379 @@
+﻿namespace DataStructures.Tests.Arrays.Sub
+{
+    using System;
+    using DataStructures.Arrays.Sub;
+    using NUnit.Framework;
+
+    public class StaticArrayTests
+    {
+        private StaticArray<int> _array;
+        private readonly int _capacity = 5;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _array = new StaticArray<int>(_capacity);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Size_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Size;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
+        }
+
+        [Test]
+        public void IsEmpty_WhenArrayIsEmpty_ShouldReturnTrue()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_array.IsEmpty, Is.EqualTo(true));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void IsEmpty_WhenArrayIsNotEmpty_ShouldReturnFalse(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.IsEmpty;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsFull_WhenArrayIsFull_ShouldReturnTrue()
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.IsFull;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsFull_WhenArrayIsNotFull_ShouldReturnFalse()
+        {
+            // Arrange & Act & Assert
+            Assert.That(_array.IsFull, Is.EqualTo(false));
+        }
+        
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void Get_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _array.Get(index));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        public void Get_WhenIndexIsNotOutOfRange_ShouldGetItemAtGivenIndex(int index)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Get(index);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(index + 1));
+        }
+
+        [Test]
+        [TestCase(-1, -1)]
+        [TestCase(100, 100)]
+        public void Set_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index, int value)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _array.Set(index, value));
+        }
+
+        [Test]
+        [TestCase(0, 100)]
+        [TestCase(1, 100)]
+        [TestCase(2, 100)]
+        [TestCase(3, 100)]
+        [TestCase(4, 100)]
+        public void Set_WhenIndexIsNotOutOfRange_ShouldSetItemAtGivenIndex(int index, int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            _array.Set(index, value);
+
+            // Assert
+            Assert.That(_array.Get(index), Is.EqualTo(value));
+        }
+
+        [Test]
+        public void Add_WhenArrayIsFull_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _array.Add(100));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Add_WhenArrayIsNotFull_ShouldAddItemWithGivenValue(int range)
+        {
+            // Arrange & Act
+            for (var i = 1; i <= range; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Assert
+            Assert.That(_array.Size, Is.EqualTo(range));
+            for (var i = 1; i <= range; i++)
+            {
+                Assert.That(_array.Get(i - 1), Is.EqualTo(i));
+            }
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void Remove_WhenItemWithGivenValueDoesNotExist_ShouldNotRemoveItemAndReturnFalse(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Remove(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+            Assert.That(_array.Size, Is.EqualTo(_capacity));
+            for (var i = 1; i <= _capacity; i++)
+            {
+                Assert.That(_array.Get(i - 1), Is.EqualTo(i));
+            }
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Remove_WhenItemWithGivenValueExists_ShouldRemoveItemAndReturnTrue(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Remove(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+            Assert.That(_array.Size, Is.EqualTo(_capacity - 1));
+            for (var i = 1; i < value; i++)
+            {
+                Assert.That(_array.Get(i - 1), Is.EqualTo(i));
+            }
+            for (var i = value; i < _capacity; i++)
+            {
+                Assert.That(_array.Get(i - 1), Is.EqualTo(i + 1));
+            }
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void RemoveAt_WhenIndexIsOutOfRange_ShouldThrowOutOfRangeException(int index)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _array.RemoveAt(index));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        public void RemoveAt_WhenIndexIsNotOutOfRange_ShouldRemoveAndReturnItemAtGivenIndex(int index)
+        {
+            // Arrange
+            for (var i = 0; i < _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.RemoveAt(index);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(index));
+            Assert.That(_array.Size, Is.EqualTo(_capacity - 1));
+            for (var i = 0; i < index; i++)
+            {
+                Assert.That(_array.Get(i), Is.EqualTo(i));
+            }
+            for (var i = index; i < _capacity - 1; i++)
+            {
+                Assert.That(_array.Get(i), Is.EqualTo(i + 1));
+            }
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Clear_WhenCalled_ShouldClearArray(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            _array.Clear();
+
+            // Assert
+            Assert.That(_array.IsEmpty, Is.EqualTo(true));
+            Assert.That(_array.Size, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void IndexOf_WhenItemWithGivenValueDoesNotExist_ShouldReturnMinusOne(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.IndexOf(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void IndexOf_WhenItemWithGivenValueExists_ShouldReturnIndex(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.IndexOf(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(value - 1));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void Contains_WhenItemWithGivenValueDoesNotExist_ShouldReturnFalse(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Contains(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Contains_WhenItemWithGivenValueExists_ShouldReturnTrue(int value)
+        {
+            // Arrange
+            for (var i = 1; i <= _capacity; i++)
+            {
+                _array.Add(i);
+            }
+
+            // Act
+            var result = _array.Contains(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+    }
+}
---
+++ DataStructures/Arrays/Sub/StaticArray.cs
@@ -0,0 +1,137 @@
+﻿namespace DataStructures.Arrays.Sub
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StaticArray<T>
+    {
+        // Represent the array
+        private readonly T[] _array;
+
+        // Represent the number of items stored in the array
+        public int Size { get; private set; }
+
+        // Represent whether the array is empty or not
+        public bool IsEmpty
+        {
+            get
+            {
+                return Size == 0;
+            }
+        }
+
+        // Represent whether the array is full or not
+        public bool IsFull
+        {
+            get
+            {
+                return Size == _array.Length;
+            }
+        }
+
+        public StaticArray(int capacity)
+        {
+            _array = new T[capacity];
+            Size = 0;
+        }
+
+        // Get an item stored at a given index
+        public T Get(int index)
+        {
+            if (index < 0 || index >= Size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _array[index];
+        }
+
+        // Set an item stored at a given index
+        public void Set(int index, T value)
+        {
+            if (index < 0 || index >= Size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            _array[index] = value;
+        }
+
+        // Add an item with a given value
+        public void Add(T value)
+        {
+            if (IsFull)
+            {
+                throw new InvalidOperationException("The array is full.");
+            }
+
+            _array[Size++] = value;
+        }
+
+        // Remove an item with a given value
+        public bool Remove(T value)
+        {
+            var index = IndexOf(value);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+            return true;
+        }
+
+        // Remove an item stored at a given index
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= Size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var item = _array[index];
+            // Shift all items on the right side of the removed item to the left by 1
+            for (var i = index; i < Size - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            // Free memory of the previously last item by setting it to default
+            _array[--Size] = default;
+
+            return item;
+        }
+
+        // Clear the array
+        public void Clear()
+        {
+            // Note that the helper function can be alternatively used as follows
+            // Array.Clear(_array, 0, Size);
+            for (var i = 0; i < Size; i++)
+            {
+                _array[i] = default;
+            }
+            Size = 0;
+        }
+
+        // Return an index of an item with a given value
+        public int IndexOf(T value)
+        {
+            for (var i = 0; i < Size; i++)
+            {
+                if (Comparer<T>
+                    .Default
+                    .Compare(_array[i], value) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        // Check whether an item with a given value exists
+        public bool Contains(T value)
+        {
+            return IndexOf(value) != -1;
+        }
+    }
+}
