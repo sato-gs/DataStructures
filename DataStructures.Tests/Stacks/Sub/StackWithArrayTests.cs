6c0df9e908e0f72de0e44c76aff8d0ed985a4217
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-09-11 00:00:15 +0100

    Organize stack with array

--- DataStructures.Tests/Stacks/Sub/StackWithArrayTests.cs
+++ DataStructures.Tests/Stacks/Sub/StackWithArrayTests.cs
@@ -4,15 +4,55 @@
     using DataStructures.Stacks.Sub;
     using NUnit.Framework;
 
-    public class StackWithStaticArrayTests
+    public class StackWithArrayTests
     {
-        private StackWithStaticArray<int> _stack;
+        private StackWithArray<int> _stack;
         private readonly int _capacity = 5;
 
         [SetUp]
         public void SetUp()
         {
-            _stack = new StackWithStaticArray<int>(_capacity);
+            _stack = new StackWithArray<int>(_capacity);
+        }
+
+        [Test]
+        [TestCase(-100)]
+        [TestCase(0)]
+        public void Constructor_WhenCapacityIsLessThanOrEqualToZero_ShouldThrowInvalidOperationException(int capacity)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<InvalidOperationException>(() => new StackWithArray<int>(capacity));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(100)]
+        public void Constructor_WhenCapacityIsGreaterThanZero_ShouldNotThrowInvalidOperationException(int capacity)
+        {
+            // Arrange & Act & Assert
+            Assert.DoesNotThrow(() => new StackWithArray<int>(capacity));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Size_WhenCalled_ShouldReturnNumberOfItems(int range)
+        {
+            // Arrange
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Act
+            var result = _stack.Size;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(range));
         }
 
         [Test]
@@ -23,14 +63,22 @@
         }
 
         [Test]
-        public void IsEmpty_WhenStackIsNotEmpty_ShouldReturnFalse()
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void IsEmpty_WhenStackIsNotEmpty_ShouldReturnFalse(int range)
         {
             // Arrange
-            _stack.Push(1);
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
 
             // Act
             var result = _stack.IsEmpty;
-            
+
             // Assert
             Assert.That(result, Is.EqualTo(false));
         }
@@ -56,51 +104,6 @@
         {
             // Arrange & Act & Assert
             Assert.That(_stack.IsFull, Is.EqualTo(false));
-        }
-
-        [Test]
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        public void Count_WhenCalled_ShouldReturnNumberOfItems(int range)
-        {
-            // Arrange
-            for (var i = 1; i <= range; i++)
-            {
-                _stack.Push(i);
-            }
-
-            // Act
-            var result = _stack.Count;
-
-            // Assert
-            Assert.That(result, Is.EqualTo(range));
-        }
-
-        [Test]
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        public void Clear_WhenCalled_ShouldClearStack(int range)
-        {
-            // Arrange
-            for (var i = 1; i <= range; i++)
-            {
-                _stack.Push(i);
-            }
-
-            // Act
-            _stack.Clear();
-
-            // Assert
-            Assert.That(_stack.IsEmpty, Is.EqualTo(true));
-            Assert.That(_stack.Count, Is.EqualTo(0));
         }
 
         [Test]
@@ -132,6 +135,32 @@
         }
 
         [Test]
+        [TestCase(1, 1)]
+        [TestCase(1, 10)]
+        [TestCase(1, 100)]
+        [TestCase(50, 1)]
+        [TestCase(50, 10)]
+        [TestCase(50, 100)]
+        [TestCase(100, 1)]
+        [TestCase(100, 10)]
+        [TestCase(100, 100)]
+        public void Push_WhenCalled_ShouldAddItemToTopOfStack(int capacity, int range)
+        {
+            // Arrange
+            _stack = new StackWithArray<int>(capacity);
+
+            // Act
+            for (var i = 1; i <= range; i++)
+            {
+                _stack.Push(i);
+            }
+
+            // Assert
+            Assert.That(_stack.Peek(), Is.EqualTo(range));
+            Assert.That(_stack.Size, Is.EqualTo(range));
+        }
+
+        [Test]
         public void Pop_WhenStackIsEmpty_ShouldThrowInvalidOperationException()
         {
             // Arrange & Act & Assert
@@ -151,46 +180,37 @@
             {
                 _stack.Push(i);
             }
-            var prevSize = _stack.Count;
+            var prevSize = _stack.Size;
 
             // Act
             var result = _stack.Pop();
 
             // Assert
             Assert.That(result, Is.EqualTo(range));
-            Assert.That(_stack.Count, Is.EqualTo(prevSize - 1));
+            Assert.That(_stack.Size, Is.EqualTo(prevSize - 1));
         }
 
         [Test]
-        public void Push_WhenStackIsFull_ShouldThrowInvalidOperationException()
-        {
-            // Arrange
-            for (var i = 1; i <= _capacity; i++)
-            {
-                _stack.Push(i);
-            }
-
-            // Act & Assert
-            Assert.Throws<InvalidOperationException>(() => _stack.Push(100));
-        }
-
-        [Test]
+        [TestCase(0)]
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(3)]
         [TestCase(4)]
         [TestCase(5)]
-        public void Push_WhenStackIsNotFull_ShouldAddItemToTopOfStack(int range)
+        public void Clear_WhenCalled_ShouldClearStack(int range)
         {
-            // Arrange & Act
+            // Arrange
             for (var i = 1; i <= range; i++)
             {
                 _stack.Push(i);
             }
 
+            // Act
+            _stack.Clear();
+
             // Assert
-            Assert.That(_stack.Peek(), Is.EqualTo(range));
-            Assert.That(_stack.Count, Is.EqualTo(range));
+            Assert.That(_stack.IsEmpty, Is.EqualTo(true));
+            Assert.That(_stack.Size, Is.EqualTo(0));
         }
     }
 }
